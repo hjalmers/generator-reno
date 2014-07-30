8f68e135b645f6fb59b47a17413fc9dc72921256
Social Thrills <drift@socialthrills.se>
2014-07-30 10:13:25 +0200

    first try of generating service

--- service/index.js
+++ service/index.js
@@ -20,7 +20,7 @@ util.inherits(ServiceGenerator, yeoman.generators.NamedBase);
 
 ServiceGenerator.prototype.files = function files() {
   this.template('service.js', 'app/services/'+this.name+'/index.js');
-  this.template('spec.js', 'app/service/'+this.name+'/index.spec.js');
+  this.template('spec.js', 'app/services/'+this.name+'/index.spec.js');
 
   this.log.writeln(' creating'.green + ' %s','files');
 };
