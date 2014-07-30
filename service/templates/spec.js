b010cc0735038c52565465115dc7407510e61c84
Social Thrills <drift@socialthrills.se>
2014-07-30 10:51:51 +0200

    fixed generated test

--- service/templates/spec.js
+++ service/templates/spec.js
@@ -1,10 +1,10 @@
-describe('app.<%= name %>', function () {
+describe('Newly generated service "app.<%= name %>"', function () {
     'use strict';
 
     beforeEach(module('app.config'));
     beforeEach(module('app.<%= name %>'));
 
-    it('should assign config to local variable', inject(function (config, <%= name %>) {
+    it('should assign value to local variable', inject(function (config, <%= name %>) {
         expect(<%= name %>.<%= name %>).toEqual('Just created!');
     }));
 });
