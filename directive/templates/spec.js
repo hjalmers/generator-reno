0bfa41f957d391907177fe66fbb2706d1aff935c
Social Thrills <drift@socialthrills.se>
2014-07-31 12:13:12 +0200

    directive generation

--- directive/templates/spec.js
+++ directive/templates/spec.js
@@ -1,16 +1,17 @@
 describe('<%= name %>', function () {
+  'use strict';
 
   var $compile, $templateCache, outerScope, scope, element;
 
   beforeEach(function () {
-    module('<%= appname %>');
+    module('app');
     inject(function ($rootScope, _$compile_, _$templateCache_) {
       outerScope = $rootScope.$new();
       $compile = _$compile_;
       $templateCache = _$templateCache_;
     });
 
-    $templateCache.put('directive/<%= name %>/<%= name %>.html', '<div></div>');
+    $templateCache.put('<%= modulename %>/<%= name %>.tpl.html', '<div></div>');
     element = $compile('<<%= name %>></<%= name %>>')(outerScope);
     outerScope.$digest();
     scope = element.isolateScope();
