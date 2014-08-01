04435ff6f950930df51f0679f3f99332d5273a6e
Social Thrills <drift@socialthrills.se>
2014-08-01 10:36:50 +0200

    i am hunting the bower folder error

--- app/index.js
+++ app/index.js
@@ -5,18 +5,12 @@ var yeoman = require('yeoman-generator');
 
 var CgangularGenerator = module.exports = function CgangularGenerator(args, options, config) {
   yeoman.generators.Base.apply(this, arguments);
-
-  this.on('end', function () {
-    this.installDependencies({ skipInstall: options['skip-install'] });
-  });
-
-  this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
 };
 
 util.inherits(CgangularGenerator, yeoman.generators.Base);
 
 
-/*CgangularGenerator.prototype.askFor = function askFor() {
+CgangularGenerator.prototype.prompting = function () {
   var cb = this.async();
 
   var prompts = [{
@@ -30,10 +24,18 @@ util.inherits(CgangularGenerator, yeoman.generators.Base);
 
     cb();
   }.bind(this));
-};*/
+};
 
-CgangularGenerator.prototype.app = function app() {
-  this.directory('skeleton/','./');
+CgangularGenerator.prototype.initializing = function () { 
+    this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
+};
+CgangularGenerator.prototype.writing = function () {
+    this.directory('skeleton/','./');
+    this.tempalate('skeleton/package.json', './packagen.json');
+};
+
+CgangularGenerator.prototype.install = function (){
+    this.installDependencies({ skipInstall: options['skip-install'] });
 };
 
 CgangularGenerator.prototype.end = function (){
