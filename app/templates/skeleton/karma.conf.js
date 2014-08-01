6012a47c04c3faf823e4e974e1ab9bd18d2ba3b8
Social Thrills <drift@socialthrills.se>
2014-08-01 11:57:22 +0200

    Quick n Dirty

--- app/templates/skeleton/karma.conf.js
+++ app/templates/skeleton/karma.conf.js
@@ -22,7 +22,7 @@ module.exports = function ( karma ) {
     exclude: [
       'src/assets/**/*.js'
     ],
-    frameworks: [ 'jasmine', 'sinon-chai' ],
+    /*frameworks: [ 'jasmine', 'sinon-chai' ],*/
     plugins: [ 'karma-sinon-chai', 'karma-html-reporter', 'karma-jasmine', 'karma-coverage', 'karma-firefox-launcher', 'karma-chrome-launcher', 'karma-phantomjs-launcher' ],
     // preprocessors: {
     //     'src/**/*.js': 'coverage'
@@ -61,8 +61,8 @@ module.exports = function ( karma ) {
      * the aesthetic advantage of not launching a browser every time you save.
      */
     browsers: [
-      'PhantomJS',
-      //'Chrome'
+      //'PhantomJS',
+      'Chrome'
       //,'Firefox'
       //,'ChromeCanary'
     ]
