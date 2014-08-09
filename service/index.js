5c519274536df39643d488aacfbff7a0391925a5
Peter Helenefors <peterhelenefors@Peters-MacBook-Pro.local>
2014-08-09 12:51:02 +0200

    added support for adding service to existing module

--- service/index.js
+++ service/index.js
@@ -18,11 +18,51 @@ var ServiceGenerator = module.exports = function ServiceGenerator(args, options,
 
 util.inherits(ServiceGenerator, yeoman.generators.NamedBase);
 
-ServiceGenerator.prototype.files = function files() {
-  this.template('service.js', 'src/app/services/'+this.name+'/index.js');
-  this.template('spec.js', 'src/app/services/'+this.name+'/index.spec.js');
+DirectiveGenerator.prototype.askFor = function askFor() {
+  var cb = this.async();
 
-  cgUtils.addToJsFileAsArrayValue('src/app/app.js', '\'module app.' + this.name + ' from "app.services.'+this.name+'.index";\'', cgUtils.PARTIAL_MODULE_MARKER,'    ');
+  var prompts = [{
+    name: 'amdmodule',
+    message: 'Enter a module path or leave blank to create new (i.e. src/app/folder/module)?',
+    validate: function(moduleName){
+      if(moduleName !== '')
+      {
+        var fullPath = ps.cwd() + '/' + moduleName + '.js';
+        if(fs.existsSync(fullPath))
+        {
+          return true;
+        }
+        return 'Can not find the specified module: ' + fullPath;
+      }
+      return true;
+    }
+  }];
+
+  this.prompt(prompts, function (props) {
+    this.needpartial = props.needpartial;
+    if(props.amdmodule){
+      this.amdmodule = props.amdmodule;                         //  src/app/form/field
+    }
+    this.moduledir = this.amdmodule.substring(0, this.amdmodule.lastIndexOf('/'))         //  src/app/form
+    this.modulepath = ps.cwd() + '/' + this.amdmodule + '.js';    //  /..project/src/app/form/field
+
+    this.dottedmoduledir = this.moduledir.replace('src', '..');                               //  ../app/form
+    
+    this.modulename = this.moduledir.substring(this.moduledir.lastIndexOf('/') + 1, this.moduledir.length); //form
+    cb();
+  }.bind(this));
+};
+
+servicegenerator.prototype.files = function files() {
+  if(this.amdmodule){
+    cgUtils.chainTemplate(this.amdmodule,  __dirname + '/templates/directive.js', { name: this.name, modulename: this.modulename, markupname: this.markupname });
+    this.log.writeln(' updating'.green + ' %s',this.amdmodule);       
+    this.template('spec.js', this.moduledir+ '/' + this.name + '.spec.js');
+  }else{
+    this.template('service.js', 'src/app/services/'+this.name+'/index.js');
+    this.template('spec.js', 'src/app/services/'+this.name+'/index.spec.js');
+    cgUtils.addToJsFileAsArrayValue('src/app/app.js', '\'module app.' + this.name + ' from "app.services.'+this.name+'.index";\'', cgUtils.PARTIAL_MODULE_MARKER,'    ');
+  }
 
   this.log.writeln(' creating'.green + ' %s','files');
 };
